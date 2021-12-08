fa015f8c437803979ed3aff80ef0f6ac02556428
bestkoby <58789388+bestkoby@users.noreply.github.com>
2021-12-08 00:45:08 -0500

    set port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const port = process.env.PORT || 80;             //Save the port number where your server will be listening
 const path = require('path');
 const bodyParser = require("body-parser");
 
